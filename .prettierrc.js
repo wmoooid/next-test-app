6a162a94f2905fc1a12e683bb5509f8f00d8c8e0
wmoooid <threeqletters@yandex.ru>
2024-05-29 15:05:24 +0400

    Boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 200,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    arrowParens: 'always',
+};
