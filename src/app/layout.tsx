dc47e0f4a776bdde8416820c1c645023fac2b8c2
wmoooid <threeqletters@yandex.ru>
2024-05-29 17:00:27 +0400

    Revert "Base layout"
    This reverts commit 282c663104551dc640971a6a0c9f4eff051d2ff7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import Footer from '@/components/footer';
-import Header from '@/components/header';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -20,11 +17,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={inter.className}>
-                <Header />
-                <main className='container'>{children}</main>
-                <Footer />
-            </body>
+            <header></header>
+            <body className={inter.className}>{children}</body>
+            <footer></footer>
         </html>
     );
 }
